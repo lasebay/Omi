c3dc162a46fefc57c652c7c68fc112c3426d8d3f
Lase <lbayudan@gatech.edu>
2017-03-04 11:29:19 -0500

    removed header in Welcome screen

--- app/src/main/java/a404_notfound/sourceappwater/controllers/WelcomeActivity.java
+++ app/src/main/java/a404_notfound/sourceappwater/controllers/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import a404_notfound.sourceappwater.R;
 
@@ -12,6 +13,7 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_welcome);
     }
 
