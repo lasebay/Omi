29a98d02cf195028a064e91dd215a0f338b0668b
Michelle Ni <michelle_ni@yahoo.com>
2017-03-11 11:06:14 -0500

    put javadocs for Admin

--- app/src/main/java/a404_notfound/sourceappwater/model/Admin.java
+++ app/src/main/java/a404_notfound/sourceappwater/model/Admin.java
@@ -10,6 +10,9 @@ public class Admin {
     private String coordinates;
     private int loginAttemps;
 
+    /**
+     * Generic constructor
+     */
     public Admin() {
         username = "No Name";
         coordinates = "None";
@@ -17,43 +20,94 @@ public class Admin {
         loginAttemps = 0;
     }
 
+    /**
+     * Constructor for admin
+     *
+     * @param name username of Admin
+     */
     public Admin(String name) {
         this();
         username = name;
     }
 
+    /**
+     * Getter for the username
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for the username
+     * TODO: remove this
+     *
+     * @param username the new username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for the admin's coordinates
+     *
+     * @return a String with the admin's latitude and longitude
+     */
     public String getCoordinates() {
         return coordinates;
     }
 
+    /**
+     * Setter for the admin's coordinates
+     * Used when admin changes location
+     *
+     * @param coordinates the new location of the admin
+     */
     public void setCoordinates(String coordinates) {
         this.coordinates = coordinates;
     }
 
+    /**
+     * Getter for login attempts
+     *
+     * @return the number of consecutive failed login attempts
+     */
     public int getLoginAttemps() {
         return loginAttemps;
     }
 
+    /**
+     * Setter for login attempts
+     *
+     * @param loginAttemps the updated amount of failed login attempts
+     */
     public void setLoginAttemps(int loginAttemps) {
         this.loginAttemps = loginAttemps;
     }
 
+    /**
+     * Setter for the street address of the admin
+     *
+     * @param address the new current address of the admin
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * Getter for the admin's address
+     *
+     * @return the current street address of the admin
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     *
+     * @return String representation of the account type
+     */
     public String toString() {
         return "Admin";
     }
