466b7381494f266e81419a3c93683d8d3df88b8a
Lase <lbayudan@gatech.edu>
2017-04-14 12:49:41 -0400

    made necessary imports

--- app/src/main/java/a404_notfound/sourceappwater/controllers/RegisterActivity.java
+++ app/src/main/java/a404_notfound/sourceappwater/controllers/RegisterActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
