e589aa6474b32fbdfc8feecad7c8f6bbf06a839c
josh9 <josh95322@gmail.com>
2017-04-14 16:17:25 -0400

    added push notification functionality

---
+++ app/src/main/java/a404_notfound/sourceappwater/model/OmiNotificationService.java
@@ -0,0 +1,24 @@
+package a404_notfound.sourceappwater.model;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Handles The logic for Push Notifications across all apps
+ */
+
+public class OmiNotificationService extends FirebaseMessagingService {
+    public OmiNotificationService() {
+        super();
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+}
